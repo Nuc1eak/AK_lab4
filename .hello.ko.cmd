7eff317ca96e324345c8cab12dd05647802b2b1c
Nuc1eak <n.kletsun@gmail.com>
2020-05-13 16:56:09 +0200

    aklab4

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nikita/repos/busybox/_install/examples/test1/hello.ko := ccache arm-eabi-ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/nikita/repos/busybox/_install/examples/test1/hello.ko /home/nikita/repos/busybox/_install/examples/test1/hello.o /home/nikita/repos/busybox/_install/examples/test1/hello.mod.o ;  true
